f7d150ef14179be763b53fa4f71b58a7cf63dc9b
volhaN2000 <volhanavoichyk@gmail.com>
2022-04-18 23:48:38 +0300

    First commit

---
+++ calculator2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Dangl.Calculator;
+
+namespace calc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите выражение для вычисления");
+          var expression = Console.ReadLine();
+            while (expression.Contains("/0")) 
+            { 
+                Console.WriteLine("Нельзя делить на ноль. Попробуйте ещё раз");
+                expression = Console.ReadLine();
+            }
+            var result=Calculator.Calculate(expression);
+            Console.WriteLine("{0}={1}",expression,(result.Result));
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+        
+    
+
+
+    
+
+
+
+                 
+    
+
+
